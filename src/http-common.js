45200187f3555549c7356ec26c526cf2f390fc50
Wikan <wikan25@gmail.com>
2021-09-11 00:51:46 +0700

    edit base url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://harga-barang.herokuapp.com",
   headers: {
     "Content-type": "application/json",
     // "Access-Control-Request-Method": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
